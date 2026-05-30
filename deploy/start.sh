073297501f42e1bd2c0c819ae57e178bd0a269d8
repositorioKeta <80453750+repositorioKeta@users.noreply.github.com>
2026-05-30 23:22:47 +0100

    add compile arm and x86_64

--- deploy/start.sh
+++ deploy/start.sh
@@ -1,7 +1,27 @@
 #!/bin/bash
 
-# backend
-/app/backend/Energy_ws &
+ARCH=$(uname -m)
+
+echo "Detected architecture: $ARCH"
+
+case "$ARCH" in
+    x86_64)
+        echo "Starting ws_x86_64"
+        /app/backend/ws_x86_64 &
+        ;;
+    aarch64)
+        echo "Starting ws_arm (ARM64)"
+        /app/backend/ws_arm &
+        ;;
+    armv7l)
+        echo "Starting ws_arm (ARM32)"
+        /app/backend/ws_arm &
+        ;;
+    *)
+        echo "Unsupported architecture: $ARCH"
+        exit 1
+        ;;
+esac
 
 # nginx
 nginx -g "daemon off;"
